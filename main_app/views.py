c326239222fee348e16db6f69649f64faf4b00fd
IliDevia <neiluamaibi@mail.ru>
2024-07-16 21:00:46 +0300

    SecondVersion

--- main_app/views.py
+++ main_app/views.py
@@ -3,5 +3,16 @@ from .models import Links
 
 
 def index(request):
-    links = Links.objects.all()
-    return render(request, 'index.html', context={'links': links})
+    correct_links = Links.objects.all()
+    wrong_links = Links.objects.order_by('?')
+    nums = [i for i in range(1, 25)]
+    wrong_links = zip(nums, wrong_links)
+    print(request.GET.get('answer') == 'true')
+
+    data = {
+        'correct_links': correct_links,
+        'wrong_links': wrong_links,
+        'answer': request.GET.get('answer') == 'true'
+    }
+
+    return render(request, 'index.html', context=data)
